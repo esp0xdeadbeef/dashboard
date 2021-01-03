41cd9fbb6a3dafc689964c6e6233ead3d3e6b1ce
esp0xdeadbeef <whydoineedthis@godoityourself.com>
2021-01-03 07:31:16 +0100

    no enter for exit at the end of the scripts

--- main/write_cfg.py
+++ main/write_cfg.py
@@ -22,11 +22,11 @@ with TinyDB(file, indent=4, separators=(', ', ': ')) as db:
     tutorial_interactive_programming_languages = "tipl"
     tutorial_reverse_shell = "trs"
     db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': False,
-               'send_enter': 1, "pre_arg": "php -a", 'exec_script': 'PHP_code_with_errors', 'args': ['\nexit']})
+               'send_enter': 0, "pre_arg": "php -a", 'exec_script': 'PHP_code_with_errors', 'args': ['\nexit']})
     db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': False,
-               'send_enter': 1, 'exec_script': 'Python_code_with_errors', "pre_arg": "ipython3", 'args': ['string_example)\nexit()']})
+               'send_enter': 0, 'exec_script': 'Python_code_with_errors', "pre_arg": "ipython3", 'args': ['string_example)\nexit()']})
     db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': False,
-               'send_enter': 1, 'exec_script': 'POWERSHELL_code_with_errors', "pre_arg": "pwsh", 'args': ['\nexit']})
+               'send_enter': 0, 'exec_script': 'POWERSHELL_code_with_errors', "pre_arg": "pwsh", 'args': ['\nexit']})
     db.insert({'session_name': MAIN, 'window_name': tutorial_reverse_shell, 'same_pane': True,
                'send_enter': 0, 'exec_script': 'socat_attacker', "pre_arg": "", 'args': ['']})
     db.insert({'session_name': MAIN, 'window_name': tutorial_reverse_shell, 'same_pane': False,
