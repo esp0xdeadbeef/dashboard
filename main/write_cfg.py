1f236380b74f6c0a558e0991619bcbf0475c1ea5
esp0xdeadbeef <whydoineedthis@godoityourself.com>
2021-01-03 07:09:21 +0100

    changed config the right way

--- main/write_cfg.py
+++ main/write_cfg.py
@@ -21,11 +21,11 @@ MAIN = "MAIN"
 with TinyDB(file, indent=4, separators=(', ', ': ')) as db:
     tutorial_interactive_programming_languages = "tipl"
     tutorial_reverse_shell = "trs"
-    db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': True,
+    db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': False,
                'send_enter': 1, "pre_arg": "php -a", 'exec_script': 'PHP_code_with_errors', 'args': ['\nexit']})
     db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': False,
                'send_enter': 1, 'exec_script': 'Python_code_with_errors', "pre_arg": "ipython3", 'args': ['string_example)\nexit()']})
-    db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': True,
+    db.insert({'session_name': MAIN, 'window_name': tutorial_interactive_programming_languages, 'same_pane': False,
                'send_enter': 1, 'exec_script': 'POWERSHELL_code_with_errors', "pre_arg": "pwsh", 'args': ['\nexit']})
     db.insert({'session_name': MAIN, 'window_name': tutorial_reverse_shell, 'same_pane': True,
                'send_enter': 0, 'exec_script': 'socat_attacker', "pre_arg": "", 'args': ['']})
@@ -33,5 +33,3 @@ with TinyDB(file, indent=4, separators=(', ', ': ')) as db:
                'send_enter': 0, 'exec_script': 'socat_victim', "pre_arg": "", 'args': [""]})
     db.insert({'session_name': MAIN, 'window_name': "try_out", 'same_pane': False,
                'send_enter': 1, 'exec_script': 'functions_tryout', "pre_arg": "", 'args': [""]})
-
-    #db.insert({'session_name': MAIN, 'window_name': "tutorial2", 'same_pane': False, 'send_enter':0, 'exec_script': '', 'args': ['some_args']})
