786856e30ce89d6e4bab68fc74d4625c248eb499
esp0xdeadbeef <whydoineedthis@godoityourself.com>
2021-10-19 22:52:15 +0200

    somehow echo without parentheses doesn't work  anymore

--- scripts/PHP_code_with_errors_0001.php
+++ scripts/PHP_code_with_errors_0001.php
@@ -4,10 +4,10 @@
 /*
 this is comment
 */
-echo 5+8;
-echo this_is_an_error;
+echo (5+8); 
+echo this_is_an_error; 
 function addTwo($n){
-    return $n + 2;
+    return $n + 2; 
 }
-var_dump(addtwo(2));
+echo(var_dump(addtwo(2)));  
 <?php>
